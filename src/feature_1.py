d3a8fe75d6dd2dc7dbaaf2397ca5b54e4756b9d1
Andreas Andersen <bok.chan@gmail.com>
2023-04-25 16:48:27 +0200

    feat: add new method

--- src/feature_1.py
+++ src/feature_1.py
@@ -1,6 +1,8 @@
 class MyFeauture:
     """A very simple class""" 
 
+    def is_a_feature(self) -> bool: 
+        return True
 
     def __repr__(self) -> str:
       print("My feature is awesome")   
